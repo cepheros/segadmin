3825122b5930453570b588da0c5cd4f4419bac22
Daniel Chaves <daniel@dnaielchaves.com.br>
2013-09-16 14:31:28 -0300

    Initial Commit

--- module/Core/Module.php
+++ module/Core/Module.php
@@ -2,31 +2,30 @@
 namespace Core;
 
 use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
 
 class Module
 {
 
-	public function onBootstrap($e)
-	{
-		
-		$eventManager        = $e->getApplication()->getEventManager();
-		$moduleRouteListener = new ModuleRouteListener();
-		$moduleRouteListener->attach($eventManager);
-		
-		$e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
-			$controller      = $e->getTarget();
-			$controllerClass = get_class($controller);
-			$moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
-			$config          = $e->getApplication()->getServiceManager()->get('config');
-			if (isset($config['module_layout'][$moduleNamespace])) {
-				$controller->layout($config['module_layout'][$moduleNamespace]);
-			}
-		}, 100);
-	}
+    public function onBootstrap($e)
+    {
 
-public function getAutoloaderConfig()
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+
+        $e->getApplication()->getEventManager()->getSharedManager()
+        ->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
+            $controller      = $e->getTarget();
+            $controllerClass = get_class($controller);
+            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+            $config          = $e->getApplication()->getServiceManager()->get('config');
+            if (isset($config['module_layout'][$moduleNamespace])) {
+                $controller->layout($config['module_layout'][$moduleNamespace]);
+            }
+        }, 100);
+    }
+
+    public function getAutoloaderConfig()
     {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
@@ -39,9 +38,9 @@ public function getAutoloaderConfig()
             ),
         );
     }
-	public function getConfig()
-	{
-		return include __DIR__ . '/config/module.config.php';
-	}
-	
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
 }
